653dda24f1b6644338ec8e038a126ea34e300d66
Vincent Mutungi <mugambivince501@gmail.com>
2025-03-21 19:43:26 -0500

    Week 7 Code

--- SQLQuery1_classWeek7.sql
+++ SQLQuery1_classWeek7.sql
@@ -44,8 +44,21 @@ order by VendorName;
 select VendorID, InvoiceNumber, InvoiceTotal
 from Invoices i1
 where InvoiceTotal > (select avg(InvoiceTotal) from Invoices as i2 where i2.VendorID = i1.VendorID)
-order by VendorID, InvoiceTotal
+order by VendorID, InvoiceTotal;
 
 select VendorID, VendorName, VendorState
 from Vendors v
-where not exists (select * from Invoices as i where i.VendorID = v.VendorID)
+where not exists (select * from Invoices as i where i.VendorID = v.VendorID);
+--
+select i.VendorID, max(InvoiceDate) as LatestInvoince
+from Invoices i inner join (select top 5 VendorID, AVG(InvoiceTotal) as AvgInvoice 
+							from Invoices
+							group by VendorID
+							order by AvgInvoice desc) as v on i.VendorID = v.VendorID
+group by i.VendorID
+order by LatestInvoince desc;
+
+select VendorName, max(InvoiceDate) as LatestInvoice
+from Vendors v left join Invoices as i on v.VendorID = i.VendorID
+group by VendorName
+order by LatestInvoice desc
